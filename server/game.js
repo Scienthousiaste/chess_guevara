150d47ed39c0c4cee59726f6433df9bdef83e829
Timothée Behra <tbehra42@gmail.com>
2020-03-24 01:21:06 +0100

    chess pieces

--- server/game.js
+++ server/game.js
@@ -6,18 +6,80 @@ const populateBoard = function(board) {
 		name: "T",
 		type: "black-rook"
 	};
+	board.row[0].square[1].piece = {
+		name: "C",
+		type: "black-knight"
+	};
+	board.row[0].square[2].piece = {
+		name: "F",
+		type: "black-bishop"
+	};
+	board.row[0].square[3].piece = {
+		name: "Q",
+		type: "black-queen"
+	};
+	board.row[0].square[4].piece = {
+		name: "R",
+		type: "black-king"
+	};
+	board.row[0].square[5].piece = {
+		name: "F",
+		type: "black-bishop"
+	};
+	board.row[0].square[6].piece = {
+		name: "C",
+		type: "black-knight"
+	};
 	board.row[0].square[7].piece = {
 		name: "T",
 		type: "black-rook"
 	};
+
 	board.row[7].square[0].piece = {
 		name: "T",
 		type: "white-rook"
+	};
+	board.row[7].square[1].piece = {
+		name: "C",
+		type: "white-knight"
+	};
+	board.row[7].square[2].piece = {
+		name: "F",
+		type: "white-bishop"
+	};
+	board.row[7].square[3].piece = {
+		name: "Q",
+		type: "white-queen"
+	};
+	board.row[7].square[4].piece = {
+		name: "R",
+		type: "white-king"
+	};
+	board.row[7].square[5].piece = {
+		name: "F",
+		type: "white-bishop"
+	};
+	board.row[7].square[6].piece = {
+		name: "C",
+		type: "white-knight"
 	};
 	board.row[7].square[7].piece = {
 		name: "T",
 		type: "white-rook"
 	};
+
+	for (let i = 0; i <= 7; i++) {
+		board.row[1].square[i].piece = {
+			name: "P",
+			type: "black-pawn",
+		}
+		board.row[6].square[i].piece = {
+			name: "P",
+			type: "white-pawn",
+		}
+
+	}
+
 	return board;
 }
 
